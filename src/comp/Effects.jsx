95fa48a97a83806dcf5bfd4979e5b027d624dcf4
xhsun <xhsun@ucalgary.ca>
2017-12-14 21:02:03 -0700

    Effect for annoyance is done

--- src/comp/Effects.jsx
+++ src/comp/Effects.jsx
@@ -36,6 +36,9 @@ class Effects extends Component {
       case 0:
         scene=<Movement next={this.next}/>;
         break;
+      case 1:
+        scene=<Annoyance next={this.next}/>
+        break;
       default:
         scene=null;//TODO change this to last scene
     }
@@ -52,6 +55,81 @@ class Effects extends Component {
   }
 }
 
+//Effect for annoyance
+class Annoyance extends Component{
+  constructor(props) {
+    super(props);
+
+    this.state={
+      classes: "center center-text",
+      effect: "annoyance no-show",
+      hide: null,
+    };
+
+    this.showEffect=this.showEffect.bind(this);
+  }
+
+  //for fade out animation and hide current scence
+  exitScene=()=>{
+    var temp=this.state.classes;
+    var trigger=this.props.next;
+    temp+=' fade';
+    this.setState({
+      classes: temp,
+      hide: trigger
+    });
+  }
+
+  showEffect(){
+    var time=1500;
+    var current=this;
+    var nextEffect=this.state.effect.replace('no-show','');
+
+    this.setState({ effect: nextEffect });
+
+    setTimeout(function() {
+            current.showGlitchEffect();
+        }, time);
+    setTimeout(function() {
+            current.showFontChange();
+        }, time*1.8);
+
+    setTimeout(function(){
+      current.exitScene();
+    }, time*2.5);
+  }
+
+  showGlitchEffect=()=>{
+    var nextEffect=this.state.effect;
+    nextEffect+=' glitch';
+    this.setState({effect : nextEffect});
+  }
+
+  showFontChange=()=>{
+    var nextEffect=this.state.effect.replace('glitch','');
+    nextEffect+=' comic-san';
+    this.setState({effect : nextEffect});
+  }
+
+  render(){
+    return(
+      <div className={this.state.classes} onTransitionEnd={this.state.hide}>
+        <h1>
+          <Typist
+            startDelay={1000}
+            cursor={{ hideWhenDone: true }}
+            onTypingDone={this.showEffect}
+          >
+            Or some minor
+          </Typist>
+        </h1>
+        <div className={this.state.effect} data-text="annoyance">annoyance</div>
+      </div>
+    );
+  }
+}
+
+//Effect for movement
 class Movement extends Component {
   constructor(props) {
     super(props);
@@ -91,7 +169,7 @@ class Movement extends Component {
 
     setTimeout(function(){
       current.exitScene();
-    }, time*6);
+    }, time*5);
   }
 
   showMovement=()=>{
